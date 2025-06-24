cd77151c1c77a0710104332523c53dcbd5386fea
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-06-24 20:50:42 +0530

    ui: add display area and Buttons component to Calculator component

--- frontend/src/components/Calculator.vue
+++ frontend/src/components/Calculator.vue
@@ -1,5 +1,22 @@
-<script setup></script>
+<script setup>
+import Buttons from '@/components/Buttons.vue'
+</script>
 
-<template></template>
+<template>
+  <div class="calc-con">
+    <div class="display">
+      <p class="result">0.0</p>
+      <input class="input" type="text" />
+    </div>
+    <Buttons />
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.calc-con {
+  display: flex;
+  flex-direction: column;
+  background: greenyellow;
+  padding: 1rem;
+}
+</style>
